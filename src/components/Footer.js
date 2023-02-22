46772f3b3977fb4ae62d0aa37b52b4072f023c0b
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-02-22 02:38:32 -0800

    Small fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <div className="container">
-            <p><a href="mailto:email@chatgpt.uw.edu"><span className="material-icons">email</span> email@chatgpt.uw.edu</a></p>
-            <p><a href="tel:206-123-4567"><span class="material-icons">phone</span> 206-123-4567</a></p>
-            <p>&copy; 2023 Harold Pham, Vivian Hung, Joseph Tran, Muyang Zhou</p>
+            <div className="footer-info">
+                <p><a href="mailto:email@chatgpt.uw.edu"><span className="material-icons">email</span> email@chatgpt.uw.edu</a></p>
+                <p><a href="tel:206-123-4567"><span class="material-icons">phone</span> 206-123-4567</a></p>
+                <p>&copy; 2023 Harold Pham, Vivian Hung, Joseph Tran, Muyang Zhou</p>
             </div>
         </footer>
     );
