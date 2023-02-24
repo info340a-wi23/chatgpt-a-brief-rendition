d7c54d4f359ac2ef93308034742041e7b9408f05
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-02-23 19:56:27 -0800

    Cleaning files

--- src/components/App.js
+++ src/components/App.js
@@ -5,46 +5,19 @@ import Menu from './Menu';
 import Header from './Header'
 import HomePage from './HomePage';
 import About from './About';
-import Discussion from './Discussion';
 import DiscussionPage from './DiscussionPage';
 import Tweets from './Tweets';
 import Footer from './Footer';
-import { HeaderBar } from './HeaderBar.js';
-import { ChannelList } from './ChannelNav.js';
-import { ChatPane } from './ChatPane.js';
-import { ComposeForm } from './ComposeForm';
 import CHAT_HISTORY from '../data/chat_log.json';
 
 // OTHER IMPORTS HERE
 import USERS from '../data/users.json';
 
 export default function App(props) {
-    // TODO: React routing
-    //state
+  //state
   const [chatMessages, setChatMessages] = useState(CHAT_HISTORY);
-  const [currentUser, setCurrentUser] = 
-      useState({userId: null, userName: null, userImg: '/img/null.png'});
   //initialize as null user
-
-  const channelList = [
-    'general', 'random', 'dank-memes', 'channel-4', 'pet-pictures'
-  ]
-  const currentChannel = "general";
-
-  const addMessage = ( messageText) => {
-    const userObj = currentUser;
-    const newMessage = {
-      "userId": userObj.userId,
-      "userName": userObj.userName,
-      "userImg": userObj.userImg,
-      "text": messageText,
-      "timestamp": Date.now(),
-      "channel": currentChannel
-    } 
-
-    const updateChatMessages = [...chatMessages, newMessage];
-    setChatMessages(updateChatMessages); //update state and re-render
-  }
+  const [currentUser, setCurrentUser] = useState({userId: null, userName: null, userImg: '/img/null.png'});
 
   const loginUser = (userObj) => {
     console.log("logging in as: ", userObj.userName);
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Menu(props) {
-    // Hamburger Menu
-    // Fix href
-    // Icon
+    // Hamburger Menu add in
     return (
         <nav className='navbar-static-top'>
             <ul className="inline-menu">
+                {/* <li><div id="hamburger-menu"><a href="#"><i class="fa fa-bars" aria-label="menu"></i></a></div></li> */}
                 <li><img src={require("./img/favicon.png")} alt="A picture of the website logo."/></li>
                 <li><Link to='home'><span className="material-symbols-outlined">cottage</span></Link></li>
                 <li><Link to='about'>About</Link></li>
