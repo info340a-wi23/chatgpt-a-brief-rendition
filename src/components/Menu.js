6f4ae8606d568113c377b5fb9f031a316bfa2534
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-02-23 20:50:55 -0800

    Bootstrap install

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,8 +7,8 @@ export default function Menu(props) {
         <nav className='navbar-static-top'>
             <ul className="inline-menu">
                 {/* <li><div id="hamburger-menu"><a href="#"><i class="fa fa-bars" aria-label="menu"></i></a></div></li> */}
-                <li><img src={require("./img/favicon.png")} alt="A picture of the website logo."/></li>
-                <li><Link to='home'><span className="material-symbols-outlined">cottage</span></Link></li>
+                <li><img src="./img/favicon.png" alt="A picture of the website logo."/></li>
+                {/* <li><Link to='home'><span className="material-symbols-outlined">cottage</span></Link></li> */}
                 <li><Link to='about'>About</Link></li>
                 <li><Link to='discussion'>Discussion</Link></li>
                 <li><Link to='tweets'>Tweets</Link></li>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
