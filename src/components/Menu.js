23595e1047837ea8884780ac7415f670e3b5cf21
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-02-25 17:57:07 -0800

    Mobile First Change Save Progress

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Menu(props) {
     // Hamburger Menu add in
     return (
-        <nav className='navbar-static-top'>
-            <ul className="inline-menu">
-                {/* <li><div id="hamburger-menu"><a href="#"><i class="fa fa-bars" aria-label="menu"></i></a></div></li> */}
-                <li><img src="./img/favicon.png" alt="A picture of the website logo."/></li>
-                {/* <li><Link to='home'><span className="material-symbols-outlined">cottage</span></Link></li> */}
-                <li><Link to='about'>About</Link></li>
-                <li><Link to='discussion'>Discussion</Link></li>
-                <li><Link to='tweets'>Tweets</Link></li>
-            </ul>
+        <nav className='main-nav navbar'>
+            <div className="dropdown">
+                <ul>
+                    <li><img src="./img/favicon.png" alt="A picture of the website logo."/></li>
+                    <li><div id="hamburger-menu"><a href="#"><i className="fa fa-bars" aria-label="menu"></i></a></div></li>
+                    <li><NavLink to='home'><span className="material-symbols-outlined">cottage</span></NavLink></li>
+                    <li><NavLink to='about'>About</NavLink></li>
+                    <li><NavLink to='discussion'>Discussion</NavLink></li>
+                    <li><NavLink to='tweets'>Tweets</NavLink></li>
+
+                </ul>
+            </div>
+
+            {/* <div className='dropdown'>
+                <button type='button' className='navbar-toggler' data-toggle='dropdown'><i className="fa fa-bars" aria-label="menu"></i></button>
+                <div className='dropdown-menu'>
+                    <NavLink to='home' className='dropdown-item'><span className="material-symbols-outlined active">cottage</span></NavLink>
+                    <NavLink to='about' className='dropdown-item'>About</NavLink>
+                    <NavLink to='discussion' className='dropdown-item'>Discussion</NavLink>
+                    <NavLink to='tweets' className='dropdown-item'>Tweets</NavLink>
+                </div>
+                <img className ='site-logo' src="./img/favicon.png" alt="A picture of the website logo."/>
+            </div> */}
         </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import './test.css'
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 
