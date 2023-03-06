5b1412e89355774e849f3027ee40e53b16672118
vvnhng <vvnhng@uw.edu>
2023-03-05 20:39:32 -0800

    set up firebase app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  databaseURL: "https://chatgpt-a-brief-rendition-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
